8b5866743f4b2140830789c3215548e65f0f4680
PulkitArun <pulkitarun@gmail.com>
2017-07-12 18:14:38 +0530

    My first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel git learning
                 </div>
 
                 <div class="links">
