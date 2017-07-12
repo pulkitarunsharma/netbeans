46e02c8599650cd80f400b63674e7cf18a11ed69
pulkit <pulkit@Pulkit-Arun>
2017-07-13 01:18:21 +0530

    second resolve merge conflict

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,9 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-
                     Laravel git learning  branch merging
-
                 </div>
 
                 <div class="links">
